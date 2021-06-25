0bd553f6bcd820646578fb8b55b90081408e99d4
Lucas Porto <lucaspmv@hotmail.com>
2021-06-25 13:09:07 -0300

    Toda estrutura HTML e CSS finalizada, criação do hook useRoom, criação das funcionalidades de likes e de encerrar sala

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { NewRoom } from "./pages/NewRoom";
 import { Room } from './pages/Room';
 
 import { AuthContextProvider } from './contexts/AuthContext';
+import { AdminRoom } from './pages/AdminRoom';
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
           <Route path='/' exact component={Home}></Route>
           <Route path='/rooms/new' component={NewRoom}></Route>
           <Route path='/rooms/:id' component={Room}></Route>
+
+          <Route path='/admin/rooms/:id' component={AdminRoom}></Route>
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
